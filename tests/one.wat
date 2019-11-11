016b6b9c64e7f1f38d307535872c831f550ae388
Lorxu <sam@dirkback.com>
2019-11-10 18:48:24 -0600

    Add a basic test framework

---
+++ tests/one.wat
@@ -0,0 +1,59 @@
+;; 0 676 27 512 219 63
+(module
+  (; wasm-vk uses our declared start function as the SPIR-V entry point ;)
+  (start $main)
+  (; We declare an imported global for getting our thread index ;)
+  (import "spv" "id" (global $id i32))
+  (; This is the buffer we're allowed to read and write
+     The size declared here doesn't matter, because wasm-vk needs to be passed a buffer
+     that the user has allocated with whatever size they want ;)
+  (memory $mem 1)
+  (; 'main' is a start function, so it doesn't have any parameters or return anything ;)
+  (func $main
+    (local $ptr i32)
+    (local $val i32)
+    (; $id is an invocation index - we need a byte index, so we multiply by 4
+       (since we're storing a 4-byte number) ;)
+    (local.set $ptr
+      (i32.mul
+        (i32.const 4)
+        (global.get $id)))
+    (local.set $val
+      (i32.load (local.get $ptr)))
+    (; If this spot has a 1 in it, start looping ;)
+    (if (i32.eq (local.get $val) (i32.const 1))
+      (then
+        (loop $continue
+          (; Add one and square it until it's bigger than thirty
+             we should get 676 ;)
+          (local.set $val
+            (i32.add (local.get $val) (i32.const 1)))
+          (local.set $val
+            (i32.mul (local.get $val) (local.get $val)))
+          (br_if $continue (i32.le_u (local.get $val) (i32.const 30)))
+        )
+        (; Store 676 to the buffer and exit ;)
+        (i32.store (local.get $ptr) (local.get $val))
+        (return)
+        ))
+    (; If this spot has a 4 in it, change it to an 18,
+       so the final result should be `(18 * 12) + 3` = 219 ;)
+    (if (i32.eq (local.get $val) (i32.const 4))
+      (then (local.set $val (i32.const 18))))
+    (; If this spot has a 3 in it, return 512 ;)
+    (if (i32.eq (local.get $val) (i32.const 3))
+      (then (i32.store (local.get $ptr) (i32.const 512)))
+      (else
+        (; If this spot has a 0 in it, skip the updating logic - leave it at 0 ;)
+        (if (i32.eq (local.get $val) (i32.const 0))
+          (then (br 1))) (; Note that we branch out of the enclosing else block ;)
+        (i32.store
+          (local.get $ptr)
+          (; Essentially ` *ptr = (*ptr * 12) + 3 ` ;)
+          (i32.add
+            (i32.mul
+              (local.get $val)
+              (i32.const 12))
+            (i32.const 3)))
+        ))
+    ))
---
+++ tests/runner.rs
@@ -0,0 +1,241 @@
+//! Run all the tests in Vulkano.
+//! This assumes there's a line of the form ";; 1 2 3 4 5" at the start of each test file (.wat)
+//! with the expected output for the input [0 1 2 3 4 5 6 ..]
+
+use std::time::Instant;
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
+use vulkano::command_buffer::AutoCommandBufferBuilder;
+use vulkano::descriptor::descriptor_set::PersistentDescriptorSet;
+use vulkano::device::{Device, DeviceExtensions};
+use vulkano::instance::{Instance, InstanceExtensions, PhysicalDevice};
+use vulkano::pipeline::ComputePipeline;
+use vulkano::sync;
+use vulkano::sync::GpuFuture;
+
+use wasm_vk::*;
+
+use std::sync::Arc;
+
+const BUFFER_SIZE: usize = 65536;
+
+#[test]
+fn main() -> std::io::Result<()> {
+    let mut n_failed = 0;
+
+    use std::fs::File;
+    use std::io::prelude::*;
+    use std::io::BufReader;
+    use std::path::Path;
+
+    let p = Path::new("tests");
+    let tests = p.read_dir()?;
+    for test in tests {
+        let test = test?;
+        let test = test.path();
+        // Only run '.wat'
+        if test.extension() != Some(std::ffi::OsStr::new("wat")) {
+            continue
+        }
+        let test_name = test.file_name().unwrap().to_str().unwrap().to_string();
+        let f = File::open(test)?;
+        let mut buf_reader = BufReader::new(f);
+        let mut buf = String::new();
+        buf_reader.read_line(&mut buf)?;
+
+        let expected: Vec<u32> = buf
+            .split_whitespace()
+            .skip(1)
+            .inspect(|x| println!("{}", x))
+            .map(|x| x.trim().parse().unwrap())
+            .collect();
+
+        let mut buf = Vec::new();
+        buf_reader.read_to_end(&mut buf)?;
+        match wabt::wat2wasm(buf) {
+            Ok(binary) => {
+                let w = wasm::deserialize_buffer(&binary).unwrap();
+                let got = run_test(w);
+                if got[..expected.len()] == *expected {
+                    println!("Test {} passed", test_name);
+                } else {
+                    n_failed += 1;
+                    eprintln!(
+                        "Test {} failed, expected {:?}, got {:?}",
+                        test_name,
+                        expected,
+                        &got[..expected.len()]
+                    );
+                }
+            }
+            Err(e) => {
+                n_failed += 1;
+                eprintln!("Test {} failed verification: {:?}", test_name, e)
+            },
+        }
+    }
+
+    if n_failed == 0 {
+        Ok(())
+    } else {
+        eprintln!("{} tests failed", n_failed);
+        Err(std::io::Error::from(std::io::ErrorKind::Other))
+    }
+}
+
+fn run_test(w: wasm::Module) -> Vec<u32> {
+    // We get our WASM from the 'comp.wasm' file, which is compiled from 'comp.wat'
+    // It multiplies every number by 12 and adds 3
+    // let w = wasm::deserialize_file("examples/comp.wasm").unwrap();
+
+    // // Read SPIR-V from file instead of generating it - for debugging
+    // let spv = {
+    //     use std::io::Read;
+    //     let mut f = std::fs::File::open("examples/comp.spv").unwrap();
+    //     let mut buf = Vec::new();
+    //     f.read_to_end(&mut buf).unwrap();
+    //     buf
+    // };
+
+    // First, we generate SPIR-V
+    let spv = spirv::to_spirv(w.clone());
+
+    // We write the SPIR-V to disk so we can disassemble it later if we want
+    use std::io::Write;
+    let mut f = std::fs::File::create("examples/comp.spv").unwrap();
+    f.write_all(&spv).unwrap();
+
+    println!("Written generated spirv to 'examples/comp.spv'");
+
+    // Here's the data we'll be using, it's just BUFFER_SIZE consecutive u32s, starting at 0
+    let data_iter = 0..BUFFER_SIZE as u32;
+
+    // NO interpreting for tests
+    // // We'll interpret the WASM on the CPU, and time it
+    // let time = Instant::now();
+    // We just pass `interpret` the buffer, and the `wasm::Module`, and it gives us back the new buffer
+    // let cpu_content = interpret(&data_iter.clone().collect::<Vec<_>>(), &w);
+    // let cpu_time = Instant::now() - time;
+
+    // Now we'll run the SPIR-V on the GPU with Vulkano.
+    // This is a bunch of boilerplate, see the Vulkano examples for explanations.
+
+    let instance = Instance::new(None, &InstanceExtensions::none(), None).unwrap();
+    let physical = PhysicalDevice::enumerate(&instance).next().unwrap();
+    let queue_family = physical
+        .queue_families()
+        .find(|&q| q.supports_compute())
+        .unwrap();
+
+    let (device, mut queues) = Device::new(
+        physical,
+        physical.supported_features(),
+        &DeviceExtensions::none(),
+        [(queue_family, 0.5)].iter().cloned(),
+    )
+    .unwrap();
+
+    let queue = queues.next().unwrap();
+
+    // This is pretty messy, but is pretty much what you need to do to get your own SPIR-V loaded with Vulkano
+    let pipeline = Arc::new({
+        #[derive(Copy, Clone)]
+        struct PLayout;
+        unsafe impl vulkano::descriptor::pipeline_layout::PipelineLayoutDesc for PLayout {
+            fn num_sets(&self) -> usize {
+                1
+            }
+            fn num_bindings_in_set(&self, set: usize) -> Option<usize> {
+                assert_eq!(set, 0);
+                Some(1)
+            }
+            fn descriptor(
+                &self,
+                set: usize,
+                _binding: usize,
+            ) -> Option<vulkano::descriptor::descriptor::DescriptorDesc> {
+                assert_eq!(self.num_bindings_in_set(set), Some(1));
+                Some(vulkano::descriptor::descriptor::DescriptorDesc {
+                    ty: vulkano::descriptor::descriptor::DescriptorDescTy::Buffer(
+                        vulkano::descriptor::descriptor::DescriptorBufferDesc {
+                            // I have no idea what these do
+                            dynamic: Some(false),
+                            storage: true,
+                        },
+                    ),
+                    array_count: 1,
+                    stages: vulkano::descriptor::descriptor::ShaderStages::compute(),
+                    readonly: false,
+                })
+            }
+            fn num_push_constants_ranges(&self) -> usize {
+                0
+            }
+            fn push_constants_range(
+                &self,
+                _num: usize,
+            ) -> Option<vulkano::descriptor::pipeline_layout::PipelineLayoutDescPcRange>
+            {
+                None
+            }
+        }
+
+        let shader =
+            unsafe { vulkano::pipeline::shader::ShaderModule::new(device.clone(), &spv).unwrap() };
+
+        let entry_str = std::ffi::CString::new("main").unwrap();
+
+        let entry = unsafe { shader.compute_entry_point(&entry_str, PLayout) };
+
+        ComputePipeline::new(device.clone(), &entry, &()).unwrap()
+    });
+
+    let data_buffer =
+        CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), data_iter.clone())
+            .unwrap();
+
+    let set = Arc::new(
+        PersistentDescriptorSet::start(pipeline.clone(), 0)
+            .add_buffer(data_buffer.clone())
+            .unwrap()
+            .build()
+            .unwrap(),
+    );
+
+    let command_buffer =
+        AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family())
+            .unwrap()
+            .dispatch([1024, 1, 1], pipeline.clone(), set.clone(), ())
+            .unwrap()
+            // Finish building the command buffer by calling `build`.
+            .build()
+            .unwrap();
+
+    // We time it from command buffer submission to fence signaling
+    let time = std::time::Instant::now();
+
+    let future = sync::now(device.clone())
+        .then_execute(queue.clone(), command_buffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
+
+    future.wait(None).unwrap();
+
+    let gpu_time = Instant::now() - time;
+
+    // Here's the data the GPU got
+    let b = data_buffer.read().unwrap();
+    b.to_vec()
+
+    // Print the results (but only show the first 12 values of each):
+    // println!(
+    //     "GPU compiled in {:?}:\n\t{:?}",
+    //     gpu_time,
+    //     &data_buffer_content[..12]
+    // );
+    // println!(
+    //     "CPU interpreted in {:?}:\n\t{:?}",
+    //     cpu_time,
+    //     &cpu_content[..12]
+    // );
+}
